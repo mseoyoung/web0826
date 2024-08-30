4c01c61292504dd098eaaaed78e63d179f739e58
mseoyoung <msy-1217@naver.com>
2024-08-30 17:58:34 +0900

    8

--- cdn1/src/imdex.js
+++
@@ -1,9 +0,0 @@
-
-// import cards from "./card_data";
-// import Card from "./card";
-
-//index.html에 출력
-
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
